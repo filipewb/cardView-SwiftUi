f4eb3b7bfc864a4b2c40d5ccdbc089177fbbbd7a
github-personal <filipe.boeck@hotmail.com>
2023-03-25 10:44:59 -0300

    Initial Commit

---
+++ Cards/CardsApp.swift
@@ -0,0 +1,17 @@
+//
+//  CardsApp.swift
+//  Cards
+//
+//  Created by wolf on 25/03/23.
+//
+
+import SwiftUI
+
+@main
+struct CardsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
